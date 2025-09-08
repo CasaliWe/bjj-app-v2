be3c640951b3602ae40b546b9082818899c37cf8
Weslei Casali <103540440+CasaliWe@users.noreply.github.com>
2025-09-08 15:27:13 -0300

    att

--- src/services/auth/logout.jsx
+++ src/services/auth/logout.jsx
@@ -1,3 +1,6 @@
+import { removeAuthToken } from '../cookies/cookies';
+
 export const sair = () => {
-  alert('logout');
+  removeAuthToken();
+  window.location.href = '/login'; // Redireciona para a página de login
 };
